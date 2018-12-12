dc916f6ee63e04b005f878e56b93df205c46144a
vishalrohra <rohra.vj@gmail.com>
2018-12-12 18:53:15 -0500

    Added game restart

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -11,6 +11,7 @@ public class Hero : MonoBehaviour {
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
+    public float gameRestartDelay = 2f;
 
     [Header("Set Dynamically")]
     [SerializeField]
@@ -81,6 +82,7 @@ public class Hero : MonoBehaviour {
             if (value < 0)
             {
                 Destroy(this.gameObject);
+                Main.S.DelayedRestart(gameRestartDelay);
             }
         }
     }
