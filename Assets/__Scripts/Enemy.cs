a731fea3063dc8ec47872bfd6fb10b69959d1aff
vishalrohra <rohra.vj@gmail.com>
2018-12-12 19:24:39 -0500

    Added Swirling Enemy 1

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -11,7 +11,7 @@ public class Enemy : MonoBehaviour
     public float health = 10;
     public int score = 100; // Points earned for destroying this
 
-    private BoundsCheck bndCheck;
+    protected BoundsCheck bndCheck;
 
     void Awake()
     {
